a2fc61810a04b28e5cdfdf4265d30a84bddda068
Sukuna69x <niloygain29@gmail.com>
2025-01-10 21:06:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,5 +12,5 @@ DB_NAME = os.environ.get("DATABASE_NAME", "cphdlust")
 SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "ziplinker.net")
 SHORTLINK_API = os.environ.get("SHORTLINK_API", "4b884da539a2f5d579e2a6f805e623c7a082a3b1")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 43200)) # Add time in seconds
-IS_VERIFY = os.environ.get("IS_VERIFY", "True")
+IS_VERIFY = os.environ.get("IS_VERIFY", "False")
 TUT_VID = os.environ.get("TUT_VID", "https://t.me/ultroid_official/18") # shareus ka tut_vid he 
