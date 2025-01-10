03eb33ab13b935bfb0526f729a66a23a552e72fa
Sukuna69x <niloygain29@gmail.com>
2025-01-10 22:18:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 #Database 
 #Database [https://youtu.be/qFB0cFqiyOM?si=fVicsCcRSmpuja1A]
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://hegodal811:rsRu17pspZAcp6V7@cluster0.prsvqax.mongodb.net/?retryWrites=true&w=majority")
-DB_NAME = os.environ.get("DATABASE_NAME", "cphdlust")
+DB_NAME = os.environ.get("DATABASE_NAME", "cphdlust1234")
 
 #Shortner (token system) 
 # check my discription to help by using my refer link of shareus.io
